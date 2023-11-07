ed6073376a199638687a16357348798e031afc42
Keith Donoghue <donoghke@tcd.ie>
2023-11-07 17:54:40 +0000

    lab6_071123

---
+++ lab_6KD.py
@@ -0,0 +1,16 @@
+# Computational Methods Lab 6
+# 07th November 2023
+# Name: Keith Donoghue
+# This Program Computes the Mean Square Error
+import numpy as np 
+
+array_1 = np.array([1,2,3,4,5,]) 
+array_2 = np.array([6,7,8,9,10]) 
+
+def mean_square_error(array_1,array_2):
+    
+    answer = (np.square(array_1 - array_2))
+    mean_square = np.mean(answer)
+    return mean_square
+
+print(mean_square_error(array_1,array_2))
